51c4786ad5e34300d7cbdb1e170e616913137b92
Vladimir Kuznetsov <kuznetsovvj@phystech.edu>
2022-11-06 12:12:56 +0400

    Codeforces

--- algorithms/codeforces/1744e1.py
+++ algorithms/codeforces/1744e1.py
@@ -1,15 +1,36 @@
 # https://codeforces.com/contest/1744/problem/E1
+# https://codeforces.com/contest/1744/problem/E2
 
+import math
+
+
+def div(num):
+    res = []
+    for i in range(1, int(num**0.5)+1):
+        if num % i == 0:
+            res.append(i)
+            if i != num // i:
+                res.append(num // i)
+    return res
 
 # неверно...
 def check(seq):
     a, b, c, d = seq
-    if b <= c and 2*a <= d and a != b:
-        return f"{b} {2*a}"
-    if 2*a <= c and 2*b <= d:
-        return f"{2*a} {2*b}"
-    return "-1 -1"
+    div_a, div_b = div(a), div(b)
     
+    # переберем все возможные пары делителей a и b
+    for item_a in div_a:
+        for item_b in div_b:
+            v1 = item_a * item_b
+            v2 = a * b // v1
+            x = (c // v1) * v1
+            y = (d // v2) * v2
+
+            if x > a and y > b:
+                return f"{x} {y}"
+    
+    return "-1 -1"
+
 
 t = int(input())
 for _ in range(t):
